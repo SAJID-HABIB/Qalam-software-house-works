db241c75bab1f4532c8b3a5704fb5c58c34fa11e
Sajid Habib <sajidchughtai779@gmail.com>
2026-04-21 22:34:06 +0500

    get the data form the dummy api and show on the frontend

--- javascript-work/Advance-js/Api-practice/script.js
+++ javascript-work/Advance-js/Api-practice/script.js
@@ -1,31 +1,31 @@
-// fetch("https://fakestoreapi.com/products")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     console.log("consle me", data); // console me
-//     document.body.innerHTML +=
-//       "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
-//   });
-
 fetch("https://fakestoreapi.com/products")
-.then((res) => res.json )
-.then((data) => {
-  console.log(data);
-  document.body.innerHTML +=
-  "<pre>"+ JSON.stringify(data, null, 2) + "</pre>";
-});
+  .then((res) => res.json())
+  .then((data) => {
+    console.log("consle me", data); // console me
+    document.body.innerHTML +=
+      "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  });
+
+// fetch("https://fakestoreapi.com/products")
+// .then((res) => res.json )
+// .then((data) => {
+//   console.log(data);
+//   document.body.innerHTML +=
+//   "<pre>"+ JSON.stringify(data, null, 2) + "</pre>";
+// });
  
-// post api 
-fetch("https://fakestoreapi.com/products", {
-  method : "POST",
-  headers : {
-    "Content-type" : "application/json"
-  },
-  body: JSON.stringify({nam :"sajid", age : 20})
-})
-.then(response => response.json())
-.then((data) => {
-  console.log("data",data);
-})
+// // post api 
+// fetch("https://fakestoreapi.com/products", {
+//   method : "POST",
+//   headers : {
+//     "Content-type" : "application/json"
+//   },
+//   body: JSON.stringify({nam :"sajid", age : 20})
+// })
+// .then(response => response.json())
+// .then((data) => {
+//   console.log("data",data);
+// })
 
 // Put ApioParam
 
---
+++ javascript-work/Advance-js/Api-project-1/script.js
@@ -0,0 +1,23 @@
+fetch("https://fakestoreapi.com/products")
+.then(resolve => resolve.json() )
+.then(data => {
+  let apiData = document.getElementById("products");
+  for (let i = 0; i < 6; i++) {
+    let  result = data[i];
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <p><h2>id:</h2>${result.id}</p>
+    <p><h2>Title:</h2> ${result.title}</p>
+    <p><h3>price :</h3>${result.price}</p>
+    <p><h3>Description :</h3>${result.description}</p>
+    <p><h3>category :</h3>${result.category}</p>
+
+
+
+
+    `
+  apiData.appendChild(card);
+
+  }
+})
