7039d3c75035e04a70e3918312368d806b4f9767
Sajid Habib <sajidchughtai779@gmail.com>
2026-05-06 21:46:32 +0500

    local modules in the node js

---
+++ Node-js/local-modules/file-2-folder/folder-2.js
@@ -0,0 +1,3 @@
+export function deff(a,b) {
+  return a- b;
+}
---
+++ Node-js/local-modules/main.js
@@ -0,0 +1,5 @@
+import {add  } from "./file-1-folder/folder-1.js";
+import { deff } from "./file-2-folder/folder-2.js";
+
+console.log(add(2, 3));
+console.log(deff(2, 3));
